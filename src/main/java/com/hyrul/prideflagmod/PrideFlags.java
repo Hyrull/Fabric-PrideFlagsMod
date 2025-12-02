ef6a48072cff991fa75c79516e1f906e1c779632
Hyrul <xavier.leonard@outlook.com>
2025-12-02 02:16:13 +0100

    Removed unused import

--- src/main/java/com/hyrul/prideflagmod/PrideFlags.java
+++ src/main/java/com/hyrul/prideflagmod/PrideFlags.java
@@ -18,7 +18,6 @@ import net.minecraft.village.TradedItem;
 import net.minecraft.village.VillagerProfession;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import oshi.jna.platform.mac.SystemB;
 
 public class PrideFlags implements ModInitializer {
 		public static final String MOD_ID = "prideflagmod";
